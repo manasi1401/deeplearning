d41082a9a4c3f9256fb015f5ff4b9f4a649f6034
Paste, Manasi E <manasi.paste@mines.sdsmt.edu>
2018-04-19 23:44:16 -0600

    authors credits

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -1,4 +1,17 @@
-# This is MNIST cnn model as described on Tensor flow tutorials with estimators
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 #These imports are necessary to make python 2 interpreter behave as cloes to Python 2 as possible
 from __future__ import absolute_import
 from __future__ import division
@@ -128,7 +141,7 @@ def main(unused_argv):
         y=eval_labels,
         num_epochs=1,
         shuffle=False)
-
+        
     eval_results = mnist_classifier.evaluate(input_fn= eval_input_fn)
     print(eval_results)
 
